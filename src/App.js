9466b33106775e8c811fe93524fa8db7201f5310
DESKTOP-A20E83D\minseok <su960618@gmail.com>
2021-10-18 13:41:43 +0900

    [2021-10-18] hader Part , Text logo, nav bar 프레임 구성 작업 및 구글 폰트 적용완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { Component } from 'react';
 import './App.css';
-// import Header from "./Components/Header";
-// import Container from "./Components/Container";
-// import Footer from "./Components/Footer";
+import Header from "./Components/header";
+// import Container from "./Components/container";
+// import Footer from "./Components/footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
-        <p>Contents</p>
-        {/* <Header />
-        <Container />
-        <Footer /> */}
+        <Header />
+        {/* <Container /> */}
+        {/* <Footer /> */}
       </div>
     );
   }
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import "./header.css";
 
 class Header extends Component {
     render() {
         return (
             <div id="header">
-                <h1 className="logo">
-                    <img src="images/coffee-logo.png" alt="coffee-images" />
-                </h1>
+                <h1 id="logo">Cafe Apps</h1>
+
+                <nav className="navGroup">
+                    <ul className="navbar">
+                        <li className="Home">
+                            <a href="#">Home</a>
+                        </li>
+
+                        <li className="Info">
+                            <a href="#">Info</a>
+                        </li>
+
+                        <li className="Maps">
+                            <a href="#">Maps</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         )
     }
