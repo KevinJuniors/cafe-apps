9a2908dd43f47131490bd9fb5e7719086261fd18
DESKTOP-U172KAU\home <su960618@gmail.com>
2021-11-12 23:13:52 +0900

    [2021-11-12] footer address 일부 수정 및 CSS 코드 내 불필요한 주석 제거

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
                 <footer>
                     <address>
                         <p className="footerText">경기도 화성시 코딩대로 456</p>
-                        <p className="footerText">개발 오피스텔 프론트엔드 호</p>
+                        <p className="footerText">프론트엔드 프라자 678호</p>
                         <p className="footerText">031-123-4567</p>
                         <p className="footerText">대표 : 홍길동</p>
                         <p className="footerText">개인정보 담당 관리자 : 고길동</p>
